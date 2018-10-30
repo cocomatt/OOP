1229196742048fbcb14912dcaf1eb45ce5c3ed14
cocomatt <matt.currie@outlook.com>
2018-10-30 02:38:17 +0000

    got rid of quotes around boolean values

--- part-1-classes-and-instances.js
+++ part-1-classes-and-instances.js
@@ -10,14 +10,14 @@ class Pizza {
   }
 }
 
-let pizza1 = new Pizza('medium', 'regular', 'false');
+let pizza1 = new Pizza('medium', 'regular', false);
 console.log(pizza1.toppings); // [ "cheese" ]
 pizza1.addTopping("mushrooms");
 pizza1.addTopping("peppers");
 console.log(pizza1.toppings); // [ "cheese", "mushrooms", "peppers" ]
 console.log(pizza1);
 
-let pizza2 = new Pizza('large', 'thin', 'true');
+let pizza2 = new Pizza('large', 'thin', true);
 console.log(pizza2.toppings); // [ "cheese" ]
 pizza2.addTopping("pepperoni");
 console.log(pizza2.toppings); // [ "cheese", "pepperoni" ];
